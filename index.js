eaf7b0e23b197f4003758ec1e54cbda75a5e0ff2
Erica Holden <erica@Ericas-MacBook-Pro.local>
2019-08-17 22:31:34 -0400

    Trying to get this working with Heroku by adding a header.

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(morgan('short'));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-
+  res.header('Access-Control-Allow-Credentials', true);
   res.setHeader(
     'Access-Control-Allow-Methods',
     'GET, POST, OPTIONS, PUT, DELETE'
